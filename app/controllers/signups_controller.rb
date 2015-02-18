b750c1bf1f3233ab51d4f2eef2accdc19a9d1512
Donald Steinert <steinert.donald@gmail.com>
2015-02-17 23:16:50 -0500

    completed project; working signup form

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -3,5 +3,20 @@ class SignupsController < ApplicationController
 	def new
 		@signup = Signup.new
 	end
+	
+	def create
+		@signup = Signup.create(signup_params)
+		if @signup.save
+			redirect_to '/thanks'
+		else
+			render 'new'
+		end
+	end
+	
+	private
+
+  def signup_params
+    params.require(:signup).permit(:email)
+  end
 
 end
